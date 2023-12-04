92300e0240d530c57ea2d681320634c2325aa08f
Kirill Makhnev <kalorigen@gmail.com>
2023-12-04 13:14:50 +0300

    update

--- src/js/app.js
+++ src/js/app.js
@@ -3,3 +3,4 @@ import * as functions from './modules/functions.js';
 functions.aosInit();
 functions.burger();
 functions.toggleSearch();
+functions.modal();
--- src/js/modules/functions.js
+++ src/js/modules/functions.js
@@ -1,4 +1,6 @@
 import AOS from 'aos';
+import Inputmask from '../../../node_modules/inputmask/dist/inputmask.es6.js';
+import JustValidate from 'just-validate';
 
 export const aosInit = () => {
   AOS.init({
@@ -95,6 +97,102 @@ export const toggleSearch = () => {
     });
     searchBg.addEventListener('click', () => {
       toggleSearch('close');
+    });
+  }
+};
+
+export const modal = () => {
+  if (document.querySelector('.modal')) {
+    const modalOpenBtn = document.querySelectorAll('.modal_application__open');
+    const modal = document.querySelector('.modal_application');
+    const modalClose = modal.querySelector('.modal-body__close');
+    const modalBg = modal.querySelector('.modal__bg');
+    const modalItemName = modal.querySelector('.modal-body__item-name');
+    const modalItemImg = modal.querySelector('.modal-body__item-img img');
+    const modalThanks = document.querySelector('.modal_thanks');
+    const modalThanksClose = modalThanks.querySelector('.modal-body__close');
+    const modalThanksCloseBtn = modalThanks.querySelector('.modal-body__btn');
+    const body = document.querySelector('body');
+
+    const phoneInput = document.querySelector('#modal-phone-input');
+    let phoneMaskBooking = new Inputmask('9(999)999-99-99');
+    phoneMaskBooking.mask(phoneInput);
+
+    const toggleModal = (type) => {
+      if (type === 'close') {
+        modal.classList.remove('_open');
+        body.classList.remove('_lock');
+      } else {
+        if (modal.classList.contains('_open')) {
+          body.classList.remove('_lock');
+          modal.classList.remove('_open');
+        } else {
+          body.classList.add('_lock');
+          modal.classList.add('_open');
+        }
+      }
+    };
+
+    const validate = new JustValidate('#modal-body-form');
+    validate
+      .addField('#modal-name-input', [
+        {
+          rule: 'required',
+        },
+      ])
+      .addField('#modal-phone-input', [
+        {
+          rule: 'required',
+        },
+      ])
+      .addField('#modal-email-input', [
+        {
+          rule: 'required',
+        },
+        {
+          rule: 'email',
+        },
+      ])
+      .addField('#modal-checkbox', [
+        {
+          rule: 'required',
+        },
+      ])
+      .onSuccess((event) => {
+        toggleModal();
+        modalThanks.classList.add('_open');
+        body.classList.add('_lock');
+        event.target.reset();
+        setTimeout(() => {
+          modalThanks.classList.remove('_open');
+          body.classList.remove('_lock');
+        }, 6000);
+      });
+
+    modalOpenBtn.forEach((item) => {
+      item.addEventListener('click', () => {
+        const name = item.dataset.name;
+        const img = item.dataset.img;
+
+        modalItemName.innerHTML = name;
+        modalItemImg.src = img;
+
+        toggleModal();
+      });
+    });
+    modalBg.addEventListener('click', () => {
+      toggleModal('close');
+    });
+    modalClose.addEventListener('click', () => {
+      toggleModal('close');
+    });
+    modalThanksClose.addEventListener('click', () => {
+      modalThanks.classList.remove('_open');
+      body.classList.remove('_lock');
+    });
+    modalThanksCloseBtn.addEventListener('click', () => {
+      modalThanks.classList.remove('_open');
+      body.classList.remove('_lock');
     });
   }
 };
